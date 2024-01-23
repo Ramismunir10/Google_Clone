b4668a0b1946cb626d45ee98b274cbdb4776538c
ramismunir10 <ramismunir10@gmail.com>
2024-01-23 23:48:22 +0500

    Flutter Google Clone

---
+++ google_clone/lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayoutScreen extends StatelessWidget {
+  const ResponsiveLayoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder:  )
+  }
+}
